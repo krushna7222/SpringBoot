645ba78a665cae2f9696f31f165b11301da9c909
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-27 22:31:04 +0530

    Create ApiResponse.java

---
+++ Student_Managment_System/src/main/java/com/org/utils/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.org.utils;
+
+public class ApiResponse<T> {
+
+    private int statusCode;
+    private T data;
+    private String message;
+    private boolean success;
+
+    public ApiResponse(int statusCode, T data, String message) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+}
