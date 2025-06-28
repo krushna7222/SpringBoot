ca23288bcbfcef6168f454afd364fbd0f711dc1b
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-28 22:34:29 +0530

    Create RestApiWithJpaApplicationTests.java

---
+++ RestAPI_With_JPA/src/test/java/com/respAPI/RestApiWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.respAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
