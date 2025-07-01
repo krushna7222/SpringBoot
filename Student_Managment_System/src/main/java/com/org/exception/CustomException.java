2e712bde61c56603f9193ce6acc3864d08b7def2
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-01 23:41:42 +0530

    Create CustomException.java

---
+++ Student_Managment_System/src/main/java/com/org/exception/CustomException.java
@@ -0,0 +1,23 @@
+package com.org.exception;
+
+import java.util.List;
+
+public class CustomException extends RuntimeException {
+    private int statusCode;
+    private List<String> errors;
+
+    public CustomException(String message, int statusCode, List<String> errors) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
+
